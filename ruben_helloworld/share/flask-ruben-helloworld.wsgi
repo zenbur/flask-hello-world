1e3fedd49ef0b05fc9cec8eb82fded5a142ae236
Ruben Louis <rubenlouis@MacBook-Pro-de-Ruben.local>
2018-05-17 23:47:25 +0400

    Just adding my name, and doing some fancy renaming

---
+++ ruben_helloworld/share/flask-ruben-helloworld.wsgi
@@ -0,0 +1,1 @@
+from ruben_helloworld import app as application
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='flask-helloworld',
+    name='flask-ruben-helloworld',
     version='0.1.0',
     long_description=__doc__,
-    packages=['helloworld'],
+    packages=['ruben_helloworld'],
     package_data = {
-        'helloworld': ['share/*', 'static/*', '/templates/*'],
+        'ruben_helloworld': ['share/*', 'static/*', '/templates/*'],
     },
     zip_safe=False,
     install_requires=['Flask']
